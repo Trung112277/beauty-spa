da30d84d6f6e184202845d2869141bdca17eabd9
Trung112277 <nhattrung112277@GMAIL.COM>
2025-07-10 10:22:55 +0700

    change header and footer layout location

--- src/components/layout/mainLayout.tsx
+++ src/components/layout/mainLayout.tsx
@@ -1,14 +1,12 @@
-import { Header } from '../common/header/heeder';
-import { Footer } from '../common/footer/footer';
+import { Header } from './header';
+import { Footer } from './footer';
 
 export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative">
-      <div className="container mx-auto px-5">
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </div>
+    <div className="container mx-auto px-5">
+      <Header />
+      <main>{children}</main>
+      <Footer />
     </div>
   );
 }
