c67ce4c360e244b8d581815a640c56768de4b867
Trung112277 <nhattrung112277@GMAIL.COM>
2025-07-24 10:18:49 +0700

    fix key item copyright nav

--- src/components/feature/nav-menu/copyRightNav.tsx
+++ src/components/feature/nav-menu/copyRightNav.tsx
@@ -1,15 +1,16 @@
 import { COPY_RIGHT_NAV } from '@/constant/copyRightNav';
 import { CopyRightNavItem } from './CopyRightNavItem';
+import React from 'react';
 
 export function CopyRightNav() {
   return (
     <nav>
       <ul className="flex gap-2 items-center">
         {COPY_RIGHT_NAV.map((page, idx) => (
-          <>
-            <CopyRightNavItem key={page.url} url={page.url} title={page.title} />
+          <React.Fragment key={page.url}>
+            <CopyRightNavItem url={page.url} title={page.title} />
             {idx < COPY_RIGHT_NAV.length - 1 && <span>/</span>}
-          </>
+          </React.Fragment>
         ))}
       </ul>
     </nav>
